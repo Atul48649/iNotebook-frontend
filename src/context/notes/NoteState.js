4ad34e96a5f79cf0cf9b588f2a386448c8f6bc42
Atul Kumar <atul48649@gmail.com>
2022-01-30 16:21:19 +0530

    created Alert and AddNote component, added delete and edit font, created addNode function

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import NoteContext from "./noteContext";
 const NoteState = (props)=> {
     const notesInitial = [
         {
-            "_id": "61f4f4c8ca835e92b9513258",
+            "_id": "61f4f4c8ca835e92b9513251",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Meeting today update",
             "description": "Having a meeting @6:30 PM update",
@@ -14,7 +14,7 @@ const NoteState = (props)=> {
             "__v": 0
         },
         {
-            "_id": "61f519ebf15eaa5479bffcfb",
+            "_id": "61f519ebf15eaa5479bffcf2",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Party today",
             "description": "Having a Party @6:30 PM",
@@ -24,7 +24,7 @@ const NoteState = (props)=> {
             "__v": 0
         },
         {
-            "_id": "61f4f4c8ca835e92b9513258",
+            "_id": "61f4f4c8ca835e92b9513253",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Meeting today update",
             "description": "Having a meeting @6:30 PM update",
@@ -34,7 +34,7 @@ const NoteState = (props)=> {
             "__v": 0
         },
         {
-            "_id": "61f519ebf15eaa5479bffcfb",
+            "_id": "61f519ebf15eaa5479bffcf4",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Party today",
             "description": "Having a Party @6:30 PM",
@@ -44,7 +44,7 @@ const NoteState = (props)=> {
             "__v": 0
         },
         {
-            "_id": "61f4f4c8ca835e92b9513258",
+            "_id": "61f4f4c8ca835e92b9513255",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Meeting today update",
             "description": "Having a meeting @6:30 PM update",
@@ -54,7 +54,7 @@ const NoteState = (props)=> {
             "__v": 0
         },
         {
-            "_id": "61f519ebf15eaa5479bffcfb",
+            "_id": "61f519ebf15eaa5479bffcf6",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Party today",
             "description": "Having a Party @6:30 PM",
@@ -63,7 +63,7 @@ const NoteState = (props)=> {
             "updatedAt": "2022-01-29T10:41:47.879Z",
             "__v": 0
         },{
-            "_id": "61f4f4c8ca835e92b9513258",
+            "_id": "61f4f4c8ca835e92b9513257",
             "user": "61f4dea3b096fa815c27d664",
             "title": "Meeting today update",
             "description": "Having a meeting @6:30 PM update",
@@ -76,8 +76,35 @@ const NoteState = (props)=> {
 
     const [notes, setNotes] = useState(notesInitial);
 
+    // add a note
+    const addNote = (title, description, tag)=> {
+        console.log('Adding a new note')
+        // TODO: API call
+        const note = {
+            "_id": "61f4f4c8ca835e92b9513217",
+            "user": "61f4dea3b096fa815c27d664",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "createdAt": "2022-01-29T08:03:20.116Z",
+            "updatedAt": "2022-01-29T10:51:26.638Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    }
+
+    // delete a note
+    const deleteNote = ()=> {
+        
+    }
+
+    // update a note
+    const editNote = ()=> {
+        
+    }
+
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
